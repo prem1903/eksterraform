5a60a29a2e4c15dd40fad95cd862134ba66ba478
prem1903 <78896585+prem1903@users.noreply.github.com>
2024-03-07 10:35:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,6 @@ locals {
   cluster_name = "education-eks-${random_string.suffix.result}"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
